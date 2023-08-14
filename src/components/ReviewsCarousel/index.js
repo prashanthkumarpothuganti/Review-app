48399497a040aea6a3c6b5e734b74fb999737797
prashanthkumar <122304814+prashanthkumarpothuganti@users.noreply.github.com>
2023-08-14 14:41:10 +0530

    index.js

--- src/components/ReviewsCarousel/index.js
+++ src/components/ReviewsCarousel/index.js
@@ -1,61 +1,76 @@
-// Write your code here
-import './index.css'
 import {Component} from 'react'
 
-class ReviewsCarousel extends Component {
-  state = {activeIndex: 0}
+import './index.css'
 
-  leftArrowClicked = () => {
-    const {activeIndex} = this.state
-    if (activeIndex > 0) {
-      this.setState(prevState => ({activeIndex: prevState.activeIndex - 1}))
+class ReviewsCarousel extends Component {
+  state = {
+    activeReviewIndex: 0,
+  }
+
+  onClickRightArrow = () => {
+    const {activeReviewIndex} = this.state
+    const {reviewsList} = this.props
+
+    if (activeReviewIndex < reviewsList.length - 1) {
+      this.setState(prevState => ({
+        activeReviewIndex: prevState.activeReviewIndex + 1,
+      }))
     }
   }
 
-  rightArrowClicked = () => {
-    const {activeIndex} = this.state
-    const {reviewsData} = this.props
-    if (activeIndex < reviewsData.length - 1) {
-      this.setState(prevState => ({activeIndex: prevState.activeIndex + 1}))
+  renderActiveReview = review => {
+    const {imgUrl, username, companyName, description} = review
+
+    return (
+      <div className="review-container">
+        <img src={imgUrl} alt={username} />
+        <p className="username">{username}</p>
+        <p className="company">{companyName}</p>
+        <p className="description">{description}</p>
+      </div>
+    )
+  }
+
+  onClickLeftArrow = () => {
+    const {activeReviewIndex} = this.state
+
+    if (activeReviewIndex > 0) {
+      this.setState(prevState => ({
+        activeReviewIndex: prevState.activeReviewIndex - 1,
+      }))
     }
   }
 
   render() {
-    const {reviewsData} = this.props
-    const {activeIndex} = this.state
-    const {imgUrl, username, companyName, description} = reviewsData[
-      activeIndex
-    ]
+    const {reviewsList} = this.props
+    const {activeReviewIndex} = this.state
+    const currentReviewDetails = reviewsList[activeReviewIndex]
+
     return (
-      <div className="reviews-container">
-        <h1 className="heading"> Reviews</h1>
+      <div className="app-container">
+        <h1 className="heading">Reviews</h1>
         <div className="carousel-container">
           <button
-            className="arrow-button"
             type="button"
-            onClick={this.leftArrowClicked}
-            testid="leftArrow"
+            onClick={this.onClickLeftArrow}
+            data-testid="leftArrow"
+            className="arrow-button"
           >
             <img
               src="https://assets.ccbp.in/frontend/react-js/left-arrow-img.png"
-              alt="leftArrow"
+              alt="left arrow"
             />
           </button>
-          <div className="review-container">
-            <img src={imgUrl} alt={`${username}-avatar`} />
-            <p className="username">{username}</p>
-            <p className="company">{companyName}</p>
-            <p className="description">{description}</p>
-          </div>
+          {this.renderActiveReview(currentReviewDetails)}
           <button
-            className="arrow-button"
             type="button"
-            onClick={this.rightArrowClicked}
-            testid="rightArrow"
+            onClick={this.onClickRightArrow}
+            data-testid="rightArrow"
+            className="arrow-button"
           >
             <img
               src="https://assets.ccbp.in/frontend/react-js/right-arrow-img.png"
-              alt="rightArrow"
+              alt="right arrow"
             />
           </button>
         </div>
